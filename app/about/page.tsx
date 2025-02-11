9141b46f13b80c0fde01f12b5bc668869421002c
Nordic-International-University <info@nordicuniversity.org>
2025-02-11 16:26:05 +0500

    asasas

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -39,7 +39,7 @@ export const metadata = {
 async function getAboutData() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/about`, {
-      cache: "no-store",
+      cache: "no-cache",
     });
     if (!res.ok) throw new Error("Ma'lumotlarni olishda xatolik.");
     return res.json();
