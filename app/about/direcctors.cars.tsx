bf44df34592b42bc7bff956e402b08bb1d733014
Nordic-International-University <info@nordicuniversity.org>
2025-02-13 18:16:28 +0500

    asasas

--- app/about/direcctors.cars.tsx
+++ app/about/direcctors.cars.tsx
@@ -17,13 +17,13 @@ interface DirectorsProps {
 
 const DirectorsCard: React.FC<DirectorsProps> = ({ directors }) => {
   return (
-    <div className="grid grid-cols-3 mt-4 group justify-between gap-3">
+    <div className="grid grid-cols-3 mt-4 max-lg:grid-cols-2 max-md:grid-cols-1 group justify-between gap-3">
       {directors?.map((director) => (
         <div
           key={director.id}
-          className="w-full bg-white shadow-lg overflow-hidden border border-gray-200"
+          className="w-full relative  shadow-lg overflow-hidden border border-gray-200"
         >
-          <div className="relative w-full h-64 bg-gray-100">
+          <div className="relative w-full h-64 bg-sky-50">
             <Image
               src={`${process.env.NEXT_PUBLIC_API_URL}${director.image.file_path}`}
               alt={director.full_name}
@@ -32,13 +32,13 @@ const DirectorsCard: React.FC<DirectorsProps> = ({ directors }) => {
               className="rounded-t-lg"
             />
           </div>
-          <div className="p-4 relative">
+          <div className="p-4">
             <p className="font-bold text-lg text-black">{director.full_name}</p>
             <p className="text-sm text-gray-700 mt-1">{director.position}</p>
             <p className="text-md font-semibold text-black mt-1">
               {director.identity}
             </p>
-            <div className="absolute bottom-0  rotate-90 right-0 w-0 h-0 border-l-[40px] border-l-transparent border-t-[40px] border-t-purple-600">
+            <div className="absolute -bottom-0  rotate-90 right-0 w-0 h-0 border-l-[40px] border-l-transparent border-t-[40px] border-t-purple-600">
               <TbPdf className="text-white absolute z-10" />
             </div>
           </div>
